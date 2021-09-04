bef153e272a982826809e7058c7892c9d44e5b9b
aminubuntu <aminubuntu7@gmail.com>
2021-09-05 00:14:36 +0430

    f

--- smp2.sh
+++ smp2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if pgrep espresso
 then
-sudo criu dump -D checkpoint -t $PID --shell-job
+sudo criu dump -D checkpoint -t `pgrep espresso` --shell-job
 sleep 10s
 sudo criu restore -d -D checkpoint --shell-job
 fi
