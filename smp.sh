e4daff8ae715e64d62e139742f3faa2d13347a42
aminubuntu <aminubuntu7@gmail.com>
2021-09-05 14:57:30 +0430

    h

--- smp.sh
+++ smp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while pgrep espresso; do  sleep 1m && ls -lh esp_out1 ; done
+while pgrep espresso; do  sleep 9m && ls -lh esp_out1 ; done
--- smp2.sh
+++ smp2.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-if pgrep espresso
-then
-sudo criu dump -D checkpoint -t `pgrep espresso` --shell-job
-sleep 10s
-sudo criu restore -d -D checkpoint --shell-job
-fi
+timeout --preserve-status 48m ./smp.sh
+exit 0
