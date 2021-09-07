afbc6ad7da163573c56121eec5dee834f39ee084
aminubuntu <aminubuntu7@gmail.com>
2021-09-08 00:27:51 +0430

    l

--- esp_input_gen.py
+++ esp_input_gen.py
@@ -1,11 +1,14 @@
 f = open("./tst2", "r")
 o = open("./out", "w")
 
+#o.write('.i 608\n.o 32\n')
 o.write('.i 608\n.o 1\n')
-
+t = 0
 for i in f:
-	#o.write('{0:0608b} {1:032b}\n'.format(int(i[0:152], 16), int(i[152:], 16)))
-	o.write('{0:0608b} 1\n'.format(int(i[0:152], 16)))
+	if t > 300000:
+		#o.write('{0:0608b} {1:032b}\n'.format(int(i[0:152], 16), int(i[152:], 16)))
+		o.write('{0:0608b} 1\n'.format(int(i[0:152], 16)))
+	t += 1
 	#t = '{0:0640b}'.format(int.from_bytes(bytes.fromhex(i), 'little'))
 	#o.write(t[0:608]+ " "+ t[608:]+'\n')
 
